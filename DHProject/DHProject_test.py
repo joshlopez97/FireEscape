0b53c3a95e391d37dbef6f878982a76d75821af3
Desmen Hartono <desmen.hartono@gmail.com>
2019-05-22 08:54:53 -0700

    added my test file

--- DHProject/DHProject_test.py
+++ DHProject/DHProject_test.py
@@ -5,7 +5,6 @@ import time
 import json
 import random
 import tensorflow as tf
-import keras as kr
 
 try:
     from malmo import MalmoPython
@@ -188,107 +187,124 @@ def dijkstra_shortest_path(grid_obs, source, dest):
     return path_list
 
 #--------------------------------------- Main ---------------------------------------
+tf.reset_default_graph()
+
+#These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape=[1,441],dtype=tf.float32)
+W = tf.Variable(tf.random_uniform([441,4],0,0.01))
+Qout = tf.matmul(inputs1,W)
+predict = tf.argmax(Qout,1)
+
+#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape=[1,4],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+#DQN parameters <------------------
+eps = 0.1
+y = 0.99
+num_episodes = 2000
+#create lists to contain total rewards and steps per episode
+rList = []
+jList = []
+#DQN init end ----------------------------------------------------------------------
 
 #action list = north, south, west, east
 #this calculation is reliant on knowing the grid is 21x21
 action_trans = [(-21,'movenorth 1'), (21, 'movesouth 1'), (-1, 'movewest 1'), (1, 'moveeast 1')] 
+#for printing
+actionlist = {-21: 'movenorth 1', 21: 'movesouth 1', -1: 'movewest 1', 1: 'moveeast 1'}
+moveList = []
 
-#Q-table initializer
-Q = np.zeros([441, len(action_trans)]) #441 = len(grid)
+#init the tensorflow session
+with tf.Session() as sess:
+    sess.run(init)
 
-# Set learning parameters
-eps = 0.1
-lr = .8
-y = .8
-num_episodes = 150
+    # Create default Malmo objects:
+    agent_host = MalmoPython.AgentHost()
+    try:
+        agent_host.parse( sys.argv )
+    except RuntimeError as e:
+        print('ERROR:',e)
+        print(agent_host.getUsage())
+        exit(1)
+    if agent_host.receivedArgument("help"):
+        print(agent_host.getUsage())
+        exit(0)
 
-#create lists to contain total rewards and steps per episode
-rList = []
+    if agent_host.receivedArgument("test"):
+        num_repeats = 1
+    else:
+        num_repeats = num_episodes
 
-# Create default Malmo objects:
-agent_host = MalmoPython.AgentHost()
-try:
-    agent_host.parse( sys.argv )
-except RuntimeError as e:
-    print('ERROR:',e)
-    print(agent_host.getUsage())
-    exit(1)
-if agent_host.receivedArgument("help"):
-    print(agent_host.getUsage())
-    exit(0)
+    for i in range(num_repeats):
+        count = i
+        print()
+        print('Repeat %d of %d' % ( i+1, num_repeats ))
 
-if agent_host.receivedArgument("test"):
-    num_repeats = 1
-else:
-    num_repeats = 150
+        #maze size parameter (not used)
+        size = int(6 + 0.5*i)
+        my_mission = MalmoPython.MissionSpec(GetMissionXML("0", 0.4 + float(i/20.0), size), True)
 
-for i in range(num_repeats):
-    print()
-    print('Repeat %d of %d' % ( i+1, num_repeats ))
+        #setup mission to start
+        my_mission_record = MalmoPython.MissionRecordSpec()
+        my_mission.requestVideo(800, 500)
+        my_mission.setViewpoint(1)
+        # Attempt to start a mission:
+        max_retries = 3
+        my_clients = MalmoPython.ClientPool()
+        my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
 
-    #maze size parameter (not used)
-    size = int(6 + 0.5*i)
-    print("Size of maze:", size)
-    my_mission = MalmoPython.MissionSpec(GetMissionXML("0", 0.4 + float(i/20.0), size), True)
+        for retry in range(max_retries):
+            try:
+                agent_host.startMission( my_mission, my_clients, my_mission_record, 0, "%s-%d" % ('Moshe', i) )
+                break
+            except RuntimeError as e:
+                if retry == max_retries - 1:
+                    print("Error starting mission", (i+1), ":",e)
+                    exit(1)
+                else:
+                    time.sleep(2)
 
-    #setup mission to start
-    my_mission_record = MalmoPython.MissionRecordSpec()
-    my_mission.requestVideo(800, 500)
-    my_mission.setViewpoint(1)
-    # Attempt to start a mission:
-    max_retries = 3
-    my_clients = MalmoPython.ClientPool()
-    my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
-
-    for retry in range(max_retries):
-        try:
-            agent_host.startMission( my_mission, my_clients, my_mission_record, 0, "%s-%d" % ('Moshe', i) )
-            break
-        except RuntimeError as e:
-            if retry == max_retries - 1:
-                print("Error starting mission", (i+1), ":",e)
-                exit(1)
-            else:
-                time.sleep(2)
-
-    # Loop until mission starts:
-    print("Waiting for the mission", (i+1), "to start ",)
-    world_state = agent_host.getWorldState()
-    while not world_state.has_mission_begun:
-        #sys.stdout.write(".")
-        time.sleep(0.1)
+        # Loop until mission starts:
+        print("Waiting for the mission", (i+1), "to start ",)
         world_state = agent_host.getWorldState()
-        for error in world_state.errors:
-            print("Error:",error.text)
-    print()
+        while not world_state.has_mission_begun:
+            #sys.stdout.write(".")
+            time.sleep(0.1)
+            world_state = agent_host.getWorldState()
+            for error in world_state.errors:
+                print("Error:",error.text)
+        print()
 
-    #Q-learning
-    grid = load_grid(world_state)
-    start, end = find_start_end(grid) #start, end = gridIndex
+        grid = load_grid(world_state)
+        start, end = find_start_end(grid) #start, end = gridIndex
 
-    for i in range(num_episodes):
+        #DQN start-------------------------------------------------------------------------------
         #Reset environment and get first new observation
         s = start
         rAll = 0
-        done = False #done
+        done = False 
         j = 0
 
         #The Q-Table learning algorithm
         while j < 99:
-            time.sleep(0.1)
+            #time.sleep(0.1)  #add sleep here to make fire more effective
             j+=1
-            #Choose an action by greedily (with noise) picking from Q table
-            #a = np.argmax(Q[s,:] + np.random.randn(1,len(action_trans)) * (1./(i+1)))
+            #Choose an action by greedily (with e chance of random action) from the Q-network
+            a,allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(441)[s:s+1]})
 
+            #prob of eps to choose random action
             rng = np.random.randint(1, 100)
             if rng>=1 and rng<=(100*eps): #P(eps)
-                a = np.random.randint(0, len(action_trans)-1)
-            else:
-                a = np.argmax(Q[s,:])
+                a[0] = np.random.randint(0, len(action_trans)-1)
 
-            #Get new state and reward from environment
-            agent_host.sendCommand(action_trans[a][1])  #gets action of a
-            s1 = s + action_trans[a][0] #gets index of a
+            #step(a[0]) = Get new state and reward from environment
+            agent_host.sendCommand(action_trans[a[0]][1])  #gets action of a
+            s1 = s + action_trans[a[0]][0] #gets index of a
 
             #calculating immediate reward
             curPath = dijkstra_shortest_path(grid, s1, end)
@@ -297,23 +313,49 @@ for i in range(num_repeats):
                 done = True
             elif grid[s1] == 'fire':
                 r = -(len(curPath)-1)
-                r += -0.5
+                r += -1.5
             elif grid[s1] == 'redstone_block':
-                r = 10
+                r = -(len(curPath)-1)
                 done = True
             else:
                 r = -(len(curPath)-1)
 
             #Update Q-Table with new knowledge
-            Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
+            Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(441)[s1:s1+1]})
+            #Obtain maxQ' and set our target value for chosen action.
+            maxQ1 = np.max(Q1)
+            targetQ = allQ
+            targetQ[0,a[0]] = r + y*maxQ1
+            #Train our network using target and predicted Q values
+            _,W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(441)[s:s+1],nextQ:targetQ})
             rAll += r
-            s = s1
-            if done == True:
-                break
-        rList.append(rAll)
-        if done == True:
-            break
 
-    print("Score over time: " +  str(sum(rList)/num_episodes))
-    #print("Final Q-Table Values")
-    print(Q[start])
+            #for printing
+            s_diff = s - s1
+            moveList.append(actionlist[s_diff])
+            
+            #increment s to s1
+            s = s1
+
+            if done == True:
+                #Reduce chance of random action as we train the model.
+                eps = 1./((count/50) + 10)
+
+                if (count%10) == 0:
+                    print()
+                    print("Report for %d: " % count)
+                    print("Path length found: ", len(moveList))
+                    print("Move list found: ", moveList)
+                    print()
+                moveList.clear()
+                break
+
+        jList.append(j)
+        rList.append(rAll)
+        print("rList for %d: %d" %(count, rList[count]))
+        print("jList for %d: %d" %(count, jList[count]))
+        
+
+
+
+
